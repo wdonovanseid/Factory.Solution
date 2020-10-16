cd2364ac88948c77b991d0b6de3951e089d78ceb
William Donovan-Seid <wdstwo@live.com>
2020-10-16 08:34:49 -0700

    add Models/Engineer.cs

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,7 +1,18 @@
+using System.Collections.Generic;
+
 namespace Factory.Models
 {
   public class Engineer
   {
-    
+    public Engineer()
+    {
+      this.Machines = new HashSet<EngineerMachine>();
+    }
+
+    public int EngineerId { get; set; }
+    public string EngineerName { get; set; }
+    public string EngineerDetails { get; set; }
+    public virtual ICollection<EngineerMachine> Machines { get; set; }
+
   }
 }
