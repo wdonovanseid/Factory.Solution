638a93fc1b232b3a08d6f0cb0b401b9abb1b5c9d
William Donovan-Seid <wdstwo@live.com>
2020-10-16 08:37:39 -0700

    add Models/Machine.cs

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,7 +1,18 @@
+using System.Collections.Generic;
+
 namespace Factory.Models
 {
   public class Machine
   {
-    
+    public Machine()
+    {
+      this.Engineers = new HashSet<EngineerMachine>();
+    }
+
+    public int MachineId { get; set; }
+    public string MachineName { get; set; }
+    public string MachineModelNum { get; set; }
+    public virtual ICollection<EngineerMachine> Engineers { get; set; }
+
   }
 }
